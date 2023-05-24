42bafcb67964ca7ba9492d0bf02738a59c71875e
ashishb012 <ashishb0128@gmail.com>
2023-05-24 17:30:40 +0530

    added firebaseConfigKeys

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { firebaseConfigKeys } from "./firebaseConfigKeys";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfigKeys);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+// const analytics = getAnalytics(app);
+
+export { app, auth, firestore, storage };
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes";
-import { app, auth } from "./firebaseConfig";
+import { router } from "./Routes/routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-quill/dist/quill.snow.css";
